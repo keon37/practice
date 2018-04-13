bb4fe147e65248d41035054874984dfa60df000e
keon37 <rkftro246@naver.com>
2018-04-13 10:17:20 +0900

    upload form 전송

---
+++ config.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, flash, request, url_for
+# from flask import Flask, render_template, request 
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('signup.html')
+
+@app.route('/success/<name>')
+def success(name=None):
+    retunr render_template('success.html')
+    # return  'Main page %s ' % name
+
+@app.route('/login', methods=['GET' , 'POST'])
+def signup ():
+     if request.method == 'POST':
+      user = request.form['myName']
+      print("user :" + user)
+      return redirect(url_for('success', name = user))
+
+    # if request.method == 'POST' :
+    #     request.form
+
+
+if __name__ =='__main__':
+    app.debug=True
+    app.run()
---
+++ routes/signup.py
@@ -0,0 +1,7 @@
+# from flask import render_template, redirect, flash, request, url_for
+
+# @routes.route('/signup', methods=['GET' , 'POST'])
+# def signup ():
+#     return redirect(url_for('success', name = 'keonju'))
+#     # if request.method == 'POST' :
+#     #     request.form
